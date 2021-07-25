04a18f24a318a91c94528b7338498180f0fae905
Ramesh <rameshrp16@gmail.com>
2021-07-25 20:11:46 +0530

    Create project in Studio. Create LogDebug for easy debugging.

---
+++ app/src/main/java/com/rosemellow/demo/MainActivity.java
@@ -0,0 +1,32 @@
+package com.rosemellow.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.rosemellow.alternatecreditscore.SplashActivity;
+import com.rosemellow.alternatecreditscore.TermsandConditionsActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+                startActivity(new Intent(MainActivity.this, TermsandConditionsActivity.class));
+                finish();
+
+
+            }
+        }, 2000);
+    }
+}
